9f74055943821abd2617f5580445ecc8d58ffbd2
billiboi <vdauria94@gmail.com>
2026-06-11 10:24:22 +0200

    Prevent mobile header logo flash

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,6 +30,18 @@
     <meta name="robots" content="noindex,nofollow" />
   {/if}
   {@html organizationStructuredDataScript}
+	<link
+  	rel="preload"
+  	as="image"
+  	href="/brand/logo-icon.png"
+  	media="(max-width: 767px)"
+	/>
+	<link
+ 	 rel="preload"
+	  as="image"
+	  href="/brand/logo-horizontal.png"
+ 	 media="(min-width: 768px)"
+	/>
 </svelte:head>
 
 {#if !isAdminRoute}
